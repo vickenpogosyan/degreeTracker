66ebb33474557aebba7b42f09f18ff529520128f
Noel K <nbkurian11@gmail.com>
2025-12-23 15:45:27 -0500

    Initial skeleton push

---
+++ src/main/java/com/degree/tracker/DegreeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.degree.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegreeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DegreeTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/degree/tracker/DegreeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.degree.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DegreeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
