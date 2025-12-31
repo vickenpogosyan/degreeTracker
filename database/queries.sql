a3ca8e21c44232163d0f129465961cc752cbf072
Vicken Pogosyan <vickenp@outlook.com>
2025-12-30 23:51:47 -0500

    Update queries.sql

--- database/queries.sql
+++ database/queries.sql
@@ -1,5 +1,10 @@
 -- Returns user info for authentication
-SELECT id, email, password
+SELECT id, email, password_hash
 FROM "app_user"
 WHERE email = ?; 
 
+-- Returns degree info
+SELECT d.name, d.honours, d.total_credits
+FROM app_user u
+JOIN degree d ON u.degree_id = d.id
+WHERE u.id = ?;
